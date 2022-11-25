7c3b810c422eb8bd02c0a55fadfba0205a88fd6a
UtoKing <utoking@outlook.com>
2022-11-25 23:19:33 +0800

    update AVX

--- Project-4-Matrix-Multiplication/main.c
+++ Project-4-Matrix-Multiplication/main.c
@@ -1,15 +1,15 @@
 #include "include/main.h"
 
 int main() {
-  size_t n = 1024;
-  printf(".");
-  float p1[n];
+  size_t n = 4096;
+  printf("?\n");
+  float *p1 = malloc(n * n * sizeof(float));
   for (int i = 0; i < n * n; ++i) {
-    *(p1 + i) = (float)(rand() % 10);
+	*(p1 + i) = (float)(rand() % 10);
   }
-  float p2[n];
+  float *p2 = malloc(n * n * sizeof(float));
   for (int i = 0; i < n * n; ++i) {
-    *(p2 + i) = (float)(rand() % 10);
+	*(p2 + i) = (float)(rand() % 10);
   }
 
   matrix *m1 = createMatrix(p1, n, n);
@@ -19,19 +19,19 @@ int main() {
   matrix *m3;
   double duration;
 
-  // start = clock();
-  // m3 = matmul_plain(m1, m2);
-  // stop = clock();
-  // printf("First element: %f\n", *(m3->data));
-  // duration = (double)(stop - start) / CLOCKS_PER_SEC;
-  // printf("%f\n", duration);
+//   start = clock();
+//   m3 = matmul_plain(m1, m2);
+//   stop = clock();
+//   printf("First element: %f\n", *(m3->data));
+//   duration = (double)(stop - start) / CLOCKS_PER_SEC;
+//   printf("%f\n", duration);
 
-  // start = clock();
-  // m3 = matmul_improved(m1, m2);
-  // stop = clock();
-  // printf("First element: %f\n", *(m3->data));
-  // duration = (double)(stop - start) / CLOCKS_PER_SEC;
-  // printf("%f\n", duration);
+   start = clock();
+   m3 = matmul_improved(m1, m2);
+   stop = clock();
+   printf("First element: %f\n", *(m3->data));
+   duration = (double)(stop - start) / CLOCKS_PER_SEC;
+   printf("%f\n", duration);
 
   start = clock();
   m3 = matmul_simd(m1, m2);
--- Project-4-Matrix-Multiplication/src/matrixOperation.c
+++ Project-4-Matrix-Multiplication/src/matrixOperation.c
@@ -13,13 +13,13 @@ void printMatrix(matrix *m) {
 
   printf("Matrix: [\n");
   for (int i = 0; i < row * column; ++i) {
-    printf("%f", *pFloat);
-    pFloat++;
-    if ((i + 1) % column == 0) {
-      printf("\n");
-    } else {
-      printf(",");
-    }
+	printf("%f", *pFloat);
+	pFloat++;
+	if ((i + 1) % column == 0) {
+	  printf("\n");
+	} else {
+	  printf(",");
+	}
   }
   printf("]\n");
 }
@@ -32,9 +32,9 @@ matrix *createMatrix(float *data, size_t row, size_t column) {
 
   float *pFloat = m->data;
   for (int i = 0; i < row * column; ++i) {
-    *pFloat = *data;
-    pFloat++;
-    data++;
+	*pFloat = *data;
+	pFloat++;
+	data++;
   }
   return m;
 }
@@ -56,17 +56,17 @@ matrix *copyMatrix(matrix *m) {
 matrix *matmul_plain(matrix *matrix_1, matrix *matrix_2) {
   assert(matrix_1->column == matrix_2->row);
   size_t row = matrix_1->row, column = matrix_2->column,
-         middle = matrix_1->column;
+	  middle = matrix_1->column;
   float *pFloat = (float *)malloc(row * column * sizeof(float));
   for (int i = 0; i < row * column; ++i) {
-    size_t n_row = i / column, n_column = i % column;
-    float sum = 0;
-    for (int j = 0; j < middle; ++j) {
-      sum += *(matrix_1->data + n_row * middle + j) *
-             *(matrix_2->data + n_column + j * column);
-    }
-    *pFloat = sum;
-    pFloat++;
+	size_t n_row = i / column, n_column = i % column;
+	float sum = 0;
+	for (int j = 0; j < middle; ++j) {
+	  sum += *(matrix_1->data + n_row * middle + j) *
+		  *(matrix_2->data + n_column + j * column);
+	}
+	*pFloat = sum;
+	pFloat++;
   }
   pFloat -= row * column;
   return createMatrix(pFloat, row, column);
@@ -75,15 +75,15 @@ matrix *matmul_plain(matrix *matrix_1, matrix *matrix_2) {
 matrix *matmul_improved(matrix *matrix_1, matrix *matrix_2) {
   assert(matrix_1->column == matrix_2->row);
   size_t row = matrix_1->row, column = matrix_2->column,
-         middle = matrix_1->column;
+	  middle = matrix_1->column;
   float *pFloat = (float *)malloc(row * column * sizeof(float));
   for (int k = 0; k < middle; ++k) {
-    for (int i = 0; i < row; ++i) {
-      float r = *(matrix_1->data + i * row + k);
-      for (int j = 0; j < column; ++j) {
-        *(pFloat + i * row + j) += r * *(matrix_2->data + k * middle + j);
-      }
-    }
+	for (int i = 0; i < row; ++i) {
+	  float r = *(matrix_1->data + i * row + k);
+	  for (int j = 0; j < column; ++j) {
+		*(pFloat + i * row + j) += r * *(matrix_2->data + k * middle + j);
+	  }
+	}
   }
   return createMatrix(pFloat, row, column);
 }
@@ -91,23 +91,23 @@ matrix *matmul_improved(matrix *matrix_1, matrix *matrix_2) {
 matrix *matmul_simd(matrix *matrix_1, matrix *matrix_2) {
   assert(matrix_1->column == matrix_2->row);
   size_t row = matrix_1->row, column = matrix_2->column,
-         middle = matrix_1->column;
+	  middle = matrix_1->column;
   float *pFloat = (float *)malloc(row * column * sizeof(float));
   __m256 row_data[column / 8];
   for (int i = 0; i < row; ++i) {
-    for (int j = 0; j < column / 8; ++j) {
-      row_data[j] = _mm256_setzero_ps();
-    }
-    for (int j = 0; j < middle; ++j) {
-      __m256 r = _mm256_set1_ps(*(matrix_1->data + i * row + j));
-      for (int k = 0; k < column; k += 8) {
-        __m256 temp = _mm256_loadu_ps(matrix_2->data + j * middle + k);
-        row_data[k] = _mm256_fmadd_ps(r, temp, row_data[k]);
-      }
-    }
-    for (int j = 0; j < column / 8; ++j) {
-      _mm256_store_ps(pFloat + i * row + j * 8, row_data[j]);
-    }
+	for (int j = 0; j < column / 8; ++j) {
+	  row_data[j] = _mm256_setzero_ps();
+	}
+	for (int j = 0; j < middle; ++j) {
+	  __m256 r = _mm256_set1_ps(*(matrix_1->data + i * row + j));
+	  for (int k = 0; k < column / 8; ++k) {
+		__m256 temp = _mm256_loadu_ps(matrix_2->data + j * middle + k * 8);
+		row_data[k] = _mm256_fmadd_ps(r, temp, row_data[k]);
+	  }
+	}
+	for (int j = 0; j < column / 8; ++j) {
+	  _mm256_storeu_ps(pFloat + i * row + j * 8, row_data[j]);
+	}
   }
   return createMatrix(pFloat, row, column);
 }
