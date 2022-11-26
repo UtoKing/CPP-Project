c2acae1a75bf8a464e2bf7b809bd09fa41e53aab
UtoKing <miao.utoking@gmail.com>
2022-11-26 16:18:46 +0800

    update README

--- Project-4-Matrix-Multiplication/main.c
+++ Project-4-Matrix-Multiplication/main.c
@@ -1,14 +1,14 @@
 #include "include/main.h"
 
 int main() {
-  size_t n = 4096;
+  size_t n = 8192;
   float *p1 = malloc(n * n * sizeof(float));
   for (int i = 0; i < n * n; ++i) {
-	*(p1 + i) = (float)(rand() % 10);
+    *(p1 + i) = (float)(rand() % 10);
   }
   float *p2 = malloc(n * n * sizeof(float));
   for (int i = 0; i < n * n; ++i) {
-	*(p2 + i) = (float)(rand() % 10);
+    *(p2 + i) = (float)(rand() % 10);
   }
 
   matrix *m1 = createMatrix(p1, n, n);
@@ -18,71 +18,45 @@ int main() {
   matrix *m3;
   double duration;
 
-//   start = clock();
-//   m3 = matmul_plain(m1, m2);
-//   stop = clock();
-//   printf("First element: %f\n", *(m3->data));
-//   duration = (double)(stop - start) / CLOCKS_PER_SEC;
-//   printf("%f\n", duration);
+  start = clock();
+  m3 = matmul_plain(m1, m2);
+  stop = clock();
+  printf("First five element: %f, %f, %f, %f, %f,\n", *(m3->data),
+         *(m3->data + 1), *(m3->data + 2), *(m3->data + 3), *(m3->data + 4));
+  duration = (double)(stop - start) / CLOCKS_PER_SEC;
+  printf("Plain Time: %fs\n", duration);
 
-//  start = clock();
-//  m3 = matmul_improved(m1, m2);
-//  stop = clock();
-//  printf("First five element: %f, %f, %f, %f, %f,\n",
-//		 *(m3->data),
-//		 *(m3->data + 1),
-//		 *(m3->data + 2),
-//		 *(m3->data + 3),
-//		 *(m3->data + 4));
-//  duration = (double)(stop - start) / CLOCKS_PER_SEC;
-//  printf("Improved Plain Time: %fs\n", duration);
+  start = clock();
+  m3 = matmul_improved(m1, m2);
+  stop = clock();
+  printf("First five element: %f, %f, %f, %f, %f,\n", *(m3->data),
+         *(m3->data + 1), *(m3->data + 2), *(m3->data + 3), *(m3->data + 4));
+  duration = (double)(stop - start) / CLOCKS_PER_SEC;
+  printf("Improved Plain Time: %fs\n", duration);
 
   start = clock();
   m3 = matmul_simd(m1, m2);
   stop = clock();
-  printf("First five element: %f, %f, %f, %f, %f,\n",
-		 *(m3->data),
-		 *(m3->data + 1),
-		 *(m3->data + 2),
-		 *(m3->data + 3),
-		 *(m3->data + 4));
+  printf("First five element: %f, %f, %f, %f, %f,\n", *(m3->data),
+         *(m3->data + 1), *(m3->data + 2), *(m3->data + 3), *(m3->data + 4));
   duration = (double)(stop - start) / CLOCKS_PER_SEC;
   printf("SIMD Time: %fs\n", duration);
 
   start = clock();
   m3 = matmul_openmp(m1, m2);
   stop = clock();
-  printf("First five element: %f, %f, %f, %f, %f,\n",
-		 *(m3->data),
-		 *(m3->data + 1),
-		 *(m3->data + 2),
-		 *(m3->data + 3),
-		 *(m3->data + 4));
+  printf("First five element: %f, %f, %f, %f, %f,\n", *(m3->data),
+         *(m3->data + 1), *(m3->data + 2), *(m3->data + 3), *(m3->data + 4));
   duration = (double)(stop - start) / CLOCKS_PER_SEC;
   printf("OpenMP Time: %fs\n", duration);
 
   start = clock();
-  cblas_sgemm(CblasRowMajor,
-			  CblasNoTrans,
-			  CblasNoTrans,
-			  m1->row,
-			  m2->column,
-			  m1->column,
-			  1.0,
-			  m1->data,
-			  m1->column,
-			  m2->data,
-			  m2->column,
-			  0.0,
-			  m3->data,
-			  m3->column);
+  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m1->row, m2->column,
+              m1->column, 1.0, m1->data, m1->column, m2->data, m2->column, 0.0,
+              m3->data, m3->column);
   stop = clock();
-  printf("First five element: %f, %f, %f, %f, %f,\n",
-		 *(m3->data),
-		 *(m3->data + 1),
-		 *(m3->data + 2),
-		 *(m3->data + 3),
-		 *(m3->data + 4));
+  printf("First five element: %f, %f, %f, %f, %f,\n", *(m3->data),
+         *(m3->data + 1), *(m3->data + 2), *(m3->data + 3), *(m3->data + 4));
   duration = (double)(stop - start) / CLOCKS_PER_SEC;
   printf("OpenBLAS Time: %fs\n", duration);
 
--- Project-4-Matrix-Multiplication/src/matrixOperation.c
+++ Project-4-Matrix-Multiplication/src/matrixOperation.c
@@ -13,13 +13,13 @@ void printMatrix(matrix *m) {
 
   printf("Matrix: [\n");
   for (int i = 0; i < row * column; ++i) {
-	printf("%f", *pFloat);
-	pFloat++;
-	if ((i + 1) % column == 0) {
-	  printf("\n");
-	} else {
-	  printf(",");
-	}
+    printf("%f", *pFloat);
+    pFloat++;
+    if ((i + 1) % column == 0) {
+      printf("\n");
+    } else {
+      printf(",");
+    }
   }
   printf("]\n");
 }
@@ -27,32 +27,36 @@ void printMatrix(matrix *m) {
 matrix *createMatrix(float *data, size_t row, size_t column) {
   matrix *m = NULL;
 
+  if (data == NULL) {
+    fprintf(stderr, "Input data is Invalid");
+  }
+
   if (row == 0 || column == 0) {
-	fprintf(stderr, "row or/and column is 0.\n");
-	return NULL;
+    fprintf(stderr, "row or/and column is 0.\n");
+    return NULL;
   }
 
   m = (matrix *)malloc(sizeof(matrix));
 
   if (m == NULL) {
-	fprintf(stderr, "Failed to allocate memory.\n");
-	return NULL;
+    fprintf(stderr, "Failed to allocate memory.\n");
+    return NULL;
   }
   m->row = row;
   m->column = column;
   m->data = (float *)malloc(sizeof(float) * row * column);
 
   if (m->data == NULL) {
-	fprintf(stderr, "Failed to allocate memory.\n");
-	free(m);
-	return NULL;
+    fprintf(stderr, "Failed to allocate memory.\n");
+    free(m);
+    return NULL;
   }
 
   float *pFloat = m->data;
   for (int i = 0; i < row * column; ++i) {
-	*pFloat = *data;
-	pFloat++;
-	data++;
+    *pFloat = *data;
+    pFloat++;
+    data++;
   }
   return m;
 }
@@ -66,14 +70,16 @@ bool deleteMatrix(matrix *m) {
 
 matrix *copyMatrix(matrix *m) {
   if (m == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: Input is invalid.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: Input is invalid.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   } else if (m->data == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: Input has invalid data.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: Input has invalid data.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   }
 
   float *pFloat = (float *)malloc(m->row * m->column * sizeof(float));
   if (pFloat == NULL) {
-	fprintf(stderr, "Failed to allocate memory.\n");
+    fprintf(stderr, "Failed to allocate memory.\n");
   }
 
   memcpy(pFloat, m->data, m->row * m->column * sizeof(float));
@@ -83,42 +89,44 @@ matrix *copyMatrix(matrix *m) {
 
 matrix *matmul_plain(matrix *matrix_1, matrix *matrix_2) {
   if (matrix_1 == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: First input is invalid.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: First input is invalid.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   } else if (matrix_1->data == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: First input has invalid data.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr,
+            "File %s, Line %d, Function %s: First input has invalid data.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   }
 
   if (matrix_2 == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: Second input is invalid.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: Second input is invalid.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   } else if (matrix_2->data == NULL) {
-	fprintf(stderr,
-			"File %s, Line %d, Function %s: Second input has invalid data.\n",
-			__FILE__,
-			__LINE__,
-			__FUNCTION__);
+    fprintf(stderr,
+            "File %s, Line %d, Function %s: Second input has invalid data.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   }
 
   if (matrix_1->column != matrix_2->row) {
-	fprintf(stderr, "Failed to perform matrix multiplication.\n");
+    fprintf(stderr, "Failed to perform matrix multiplication.\n");
   }
 
   size_t row = matrix_1->row, column = matrix_2->column,
-	  middle = matrix_1->column;
+         middle = matrix_1->column;
 
   float *pFloat = (float *)malloc(row * column * sizeof(float));
   if (pFloat == NULL) {
-	fprintf(stderr, "Failed to allocate memory.\n");
+    fprintf(stderr, "Failed to allocate memory.\n");
   }
 
   for (int i = 0; i < row * column; ++i) {
-	size_t n_row = i / column, n_column = i % column;
-	float sum = 0;
-	for (int j = 0; j < middle; ++j) {
-	  sum += *(matrix_1->data + n_row * middle + j) *
-		  *(matrix_2->data + n_column + j * column);
-	}
-	*pFloat = sum;
-	pFloat++;
+    size_t n_row = i / column, n_column = i % column;
+    float sum = 0;
+    for (int j = 0; j < middle; ++j) {
+      sum += *(matrix_1->data + n_row * middle + j) *
+             *(matrix_2->data + n_column + j * column);
+    }
+    *pFloat = sum;
+    pFloat++;
   }
   pFloat -= row * column;
   return createMatrix(pFloat, row, column);
@@ -126,138 +134,144 @@ matrix *matmul_plain(matrix *matrix_1, matrix *matrix_2) {
 
 matrix *matmul_improved(matrix *matrix_1, matrix *matrix_2) {
   if (matrix_1 == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: First input is invalid.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: First input is invalid.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   } else if (matrix_1->data == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: First input has invalid data.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr,
+            "File %s, Line %d, Function %s: First input has invalid data.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   }
 
   if (matrix_2 == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: Second input is invalid.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: Second input is invalid.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   } else if (matrix_2->data == NULL) {
-	fprintf(stderr,
-			"File %s, Line %d, Function %s: Second input has invalid data.\n",
-			__FILE__,
-			__LINE__,
-			__FUNCTION__);
+    fprintf(stderr,
+            "File %s, Line %d, Function %s: Second input has invalid data.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   }
 
   if (matrix_1->column != matrix_2->row) {
-	fprintf(stderr, "Failed to perform matrix multiplication.\n");
+    fprintf(stderr, "Failed to perform matrix multiplication.\n");
   }
 
   size_t row = matrix_1->row, column = matrix_2->column,
-	  middle = matrix_1->column;
+         middle = matrix_1->column;
 
   float *pFloat = (float *)malloc(row * column * sizeof(float));
   if (pFloat == NULL) {
-	fprintf(stderr, "Failed to allocate memory.\n");
+    fprintf(stderr, "Failed to allocate memory.\n");
   }
 
   for (int k = 0; k < middle; ++k) {
-	for (int i = 0; i < row; ++i) {
-	  float r = *(matrix_1->data + i * row + k);
-	  for (int j = 0; j < column; ++j) {
-		*(pFloat + i * row + j) += r * *(matrix_2->data + k * middle + j);
-	  }
-	}
+    for (int i = 0; i < row; ++i) {
+      float r = *(matrix_1->data + i * row + k);
+      for (int j = 0; j < column; ++j) {
+        *(pFloat + i * row + j) += r * *(matrix_2->data + k * middle + j);
+      }
+    }
   }
   return createMatrix(pFloat, row, column);
 }
 
 matrix *matmul_simd(matrix *matrix_1, matrix *matrix_2) {
   if (matrix_1 == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: First input is invalid.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: First input is invalid.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   } else if (matrix_1->data == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: First input has invalid data.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr,
+            "File %s, Line %d, Function %s: First input has invalid data.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   }
 
   if (matrix_2 == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: Second input is invalid.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: Second input is invalid.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   } else if (matrix_2->data == NULL) {
-	fprintf(stderr,
-			"File %s, Line %d, Function %s: Second input has invalid data.\n",
-			__FILE__,
-			__LINE__,
-			__FUNCTION__);
+    fprintf(stderr,
+            "File %s, Line %d, Function %s: Second input has invalid data.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   }
 
   if (matrix_1->column != matrix_2->row) {
-	fprintf(stderr, "Failed to perform matrix multiplication.\n");
+    fprintf(stderr, "Failed to perform matrix multiplication.\n");
   }
 
   size_t row = matrix_1->row, column = matrix_2->column,
-	  middle = matrix_1->column;
+         middle = matrix_1->column;
 
   float *pFloat = (float *)malloc(row * column * sizeof(float));
   if (pFloat == NULL) {
-	fprintf(stderr, "Failed to allocate memory.\n");
+    fprintf(stderr, "Failed to allocate memory.\n");
   }
 
   __m256 row_data[column / 8];
   for (int i = 0; i < row; ++i) {
-	for (int j = 0; j < column / 8; ++j) {
-	  row_data[j] = _mm256_setzero_ps();
-	}
-	for (int j = 0; j < middle; ++j) {
-	  __m256 r = _mm256_set1_ps(*(matrix_1->data + i * row + j));
-	  for (int k = 0; k < column / 8; ++k) {
-		__m256 temp = _mm256_loadu_ps(matrix_2->data + j * middle + k * 8);
-		row_data[k] = _mm256_fmadd_ps(r, temp, row_data[k]);
-	  }
-	}
-	for (int j = 0; j < column / 8; ++j) {
-	  _mm256_storeu_ps(pFloat + i * row + j * 8, row_data[j]);
-	}
+    for (int j = 0; j < column / 8; ++j) {
+      row_data[j] = _mm256_setzero_ps();
+    }
+    for (int j = 0; j < middle; ++j) {
+      __m256 r = _mm256_set1_ps(*(matrix_1->data + i * row + j));
+      for (int k = 0; k < column / 8; ++k) {
+        __m256 temp = _mm256_loadu_ps(matrix_2->data + j * middle + k * 8);
+        row_data[k] = _mm256_fmadd_ps(r, temp, row_data[k]);
+      }
+    }
+    for (int j = 0; j < column / 8; ++j) {
+      _mm256_storeu_ps(pFloat + i * row + j * 8, row_data[j]);
+    }
   }
   return createMatrix(pFloat, row, column);
 }
 
 matrix *matmul_openmp(matrix *matrix_1, matrix *matrix_2) {
   if (matrix_1 == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: First input is invalid.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: First input is invalid.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   } else if (matrix_1->data == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: First input has invalid data.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr,
+            "File %s, Line %d, Function %s: First input has invalid data.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   }
 
   if (matrix_2 == NULL) {
-	fprintf(stderr, "File %s, Line %d, Function %s: Second input is invalid.\n", __FILE__, __LINE__, __FUNCTION__);
+    fprintf(stderr, "File %s, Line %d, Function %s: Second input is invalid.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   } else if (matrix_2->data == NULL) {
-	fprintf(stderr,
-			"File %s, Line %d, Function %s: Second input has invalid data.\n",
-			__FILE__,
-			__LINE__,
-			__FUNCTION__);
+    fprintf(stderr,
+            "File %s, Line %d, Function %s: Second input has invalid data.\n",
+            __FILE__, __LINE__, __FUNCTION__);
   }
 
   if (matrix_1->column != matrix_2->row) {
-	fprintf(stderr, "Failed to perform matrix multiplication.\n");
+    fprintf(stderr, "Failed to perform matrix multiplication.\n");
   }
 
   size_t row = matrix_1->row, column = matrix_2->column,
-	  middle = matrix_1->column;
+         middle = matrix_1->column;
 
   float *pFloat = (float *)malloc(row * column * sizeof(float));
   if (pFloat == NULL) {
-	fprintf(stderr, "Failed to allocate memory.\n");
+    fprintf(stderr, "Failed to allocate memory.\n");
   }
 
   __m256 row_data[column / 8];
   for (int i = 0; i < row; ++i) {
-	for (int j = 0; j < column / 8; ++j) {
-	  row_data[j] = _mm256_setzero_ps();
-	}
+    for (int j = 0; j < column / 8; ++j) {
+      row_data[j] = _mm256_setzero_ps();
+    }
 #pragma omp parallel for
-	for (int j = 0; j < middle; ++j) {
-	  __m256 r = _mm256_set1_ps(*(matrix_1->data + i * row + j));
+    for (int j = 0; j < middle; ++j) {
+      __m256 r = _mm256_set1_ps(*(matrix_1->data + i * row + j));
 #pragma omp parallel for
-	  for (int k = 0; k < column / 8; ++k) {
-		__m256 temp = _mm256_loadu_ps(matrix_2->data + j * middle + k * 8);
-		row_data[k] = _mm256_fmadd_ps(r, temp, row_data[k]);
-	  }
-	}
-	for (int j = 0; j < column / 8; ++j) {
-	  _mm256_storeu_ps(pFloat + i * row + j * 8, row_data[j]);
-	}
+      for (int k = 0; k < column / 8; ++k) {
+        __m256 temp = _mm256_loadu_ps(matrix_2->data + j * middle + k * 8);
+        row_data[k] = _mm256_fmadd_ps(r, temp, row_data[k]);
+      }
+    }
+    for (int j = 0; j < column / 8; ++j) {
+      _mm256_storeu_ps(pFloat + i * row + j * 8, row_data[j]);
+    }
   }
   return createMatrix(pFloat, row, column);
 }
